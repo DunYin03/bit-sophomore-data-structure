30b6b4ef8802f15f0a8c537b6a0fec7677298d78
mayicheng <1@LAPTOP-CFTV7DBR>
2022-09-09 20:31:02 +0800

    添加项目文件。

---
+++ Project1/分萝卜.c
@@ -0,0 +1,21 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//int main()
+//{
+//	int n, l, r, c;
+//	scanf("%d %d %d", &n, &l, &r);
+//	c = r - l;
+//	if (c >= n - 1)
+//		printf("%d\n", n - 1);
+//	else
+//	{
+//		int mod1, mod2;
+//		mod1 = l % n;
+//		mod2 = r % n;
+//		if (mod1 > mod2)
+//			printf("%d\n", n - 1);
+//		else
+//			printf("%d\n", mod2);
+//	}
+//	return 0;
+//}
---
+++ Project1/小白兔上台阶.c
@@ -0,0 +1,21 @@
+//#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//int deal(int n);
+//
+//int main()
+//{
+//	int n, res;
+//	scanf("%d", &n);
+//	res = deal(n);
+//	printf("%d\n", res);
+//	return 0;
+//}
+//int deal(int n)
+//{
+//	if (n == 1)
+//		return 1;
+//	else if (n == 2)
+//		return 2;
+//	else 
+//		return deal(n - 1) + deal(n - 2);
+//}
