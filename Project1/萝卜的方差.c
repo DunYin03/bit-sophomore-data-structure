14ee59a6f0ce67fbd81f72a00e2cb94a5345c432
mayicheng <1@LAPTOP-CFTV7DBR>
2022-09-10 18:30:17 +0800

    萝卜的方差（内含方差）

--- Project1/萝卜的方差.c
+++ Project1/萝卜的方差.c
@@ -1,2 +1,39 @@
 #define _CRT_SECURE_NO_WARNINGS 1
-
+#include <stdio.h>
+float variance(int a[], int n)
+{
+	float avg = 0;
+	float res = 0;
+	for (int i = 0; i < n; i++)
+		avg += a[i];
+	avg /= n;
+	for (int i = 0; i < n; i++)
+		res += (a[i] - avg) * (a[i] - avg);
+	res /= n;
+	return res;
+}
+int main()
+{
+	int n, a[1000] = { 0 };
+	int k;
+	float res;
+	scanf("%d %d", &n, &k);
+	for (int i = 0; i < n; i++)
+		scanf("%d", &a[i]);
+	res = variance(a, n);
+	if (res == 0)
+		printf("No answer!\n");
+	else
+	{
+		int a = 1, b = 2;
+		while (b * b * res <= k)
+		{
+			a++;
+			b++;
+		}
+		if (k - a * a * res > b * b * res - k)
+			printf("%d\n", b);
+		else
+			printf("%d\n", a);
+	}
+}
