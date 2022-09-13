792f582e0225ea520f062324942eb8a0d48de85e
mayicheng <3228443778@qq.com>
2022-09-13 16:00:42 +0800

    找大佬

--- Project1/程序设计方法/效率分析/8.找大佬.c
+++ Project1/程序设计方法/效率分析/8.找大佬.c
@@ -1,1 +1,63 @@
 #define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <stdlib.h>
+int n;
+int good[1000][1000];
+void guessdalao(int n); 
+int better(int a, int b);
+int main()
+{
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++)
+        for (int j = 1; j <= n; j++)
+        {
+            int t;
+            scanf("%d", &t);
+            good[i][j] = t;
+        }
+    guessdalao(n);
+    return 0;
+}
+
+
+
+int better(int a, int b)
+{
+    if (a <= 0 || a > n || b <= 0 || b > n) return -1;
+    return good[a][b];
+}
+void guessdalao(int n)
+{
+    int max = 1;
+    int flag = 0;
+    for (int i = 2; i <= n; i ++ )
+    {
+        if (better(max, i) == 0)
+            max = i;
+    }
+    for (int i = 1; i < max; i++)
+    {
+        if (better(max, i) == 0)
+            flag++;
+    }
+    for (int i = max + 1; i <= n; i++)
+    {
+        if (better(max, i) == 0)
+            flag++;
+    }
+    for (int i = 1; i < max; i++)
+    {
+        if (better(i, max) == 1)
+            flag++;
+    }
+    for (int i = max + 1; i <= n; i++)
+    {
+        if (better(i, max) == 1)
+            flag++;
+    }
+    if (flag == 0)
+        printf("%d\n", max);
+    else
+        printf("-1\n");
+    
+}
