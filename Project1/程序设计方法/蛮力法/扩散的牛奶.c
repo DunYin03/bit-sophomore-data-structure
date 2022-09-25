50169704fbfa5a74448d161083c1c5eba1f5839a
mayicheng <3228443778@qq.com>
2022-09-25 21:32:49 +0800

    dfs

--- Project1/程序设计方法/蛮力法/扩散的牛奶.c
+++ Project1/程序设计方法/蛮力法/扩散的牛奶.c
@@ -1,55 +1,70 @@
-//#define _CRT_SECURE_NO_WARNINGS 1
-////text text
-//#include<stdio.h>  
-//#include<math.h>  
-//#include<stdlib.h>  
-//int main()
-//{
-//    int n, p[200][2], k, i, j, f1, f2, f3, a, b, c;
-//    double d, total = 0;
-//    scanf("%d", &n);
-//    for (i = 0; i < n; i++)
-//    {
-//        scanf("%d %d", &p[i][0], &p[i][1]);
-//        for (j = 0; j < i; j++)
-//        {
-//            if ((p[i][0] == p[j][0]) && (p[i][1] == p[j][1]))
-//            {
-//                n--;
-//                i--;
-//            }
-//        }
-//
-//    }
-//    if (n == 1) total = 0;
-//    else
-//    {
-//        for (k = 0; k < n - 1; k++)
-//        {
-//            for (i = k + 1; i < n; i++)
-//            {
-//                a = p[k][1] - p[i][1]; //ax+by=c a=y2-y1 b=x1-x2 c=x1y2-x2y1  
-//                b = p[i][0] - p[k][0];
-//                c = p[i][0] * p[k][1] - p[k][0] * p[i][1];
-//                f1 = 0;
-//                f2 = 0;
-//                f3 = 0;
-//                for (j = 0; j < n; j++)
-//                {
-//                    if (a * p[j][0] + b * p[j][1] > c) f1++;
-//                    if (a * p[j][0] + b * p[j][1] < c) f2++;
-//                    if ((a * p[j][0] + b * p[j][1] == c) && (((p[j][0] - p[k][0]) * (p[j][0] - p[i][0]) > 0) || ((p[j][1] - p[k][1]) * (p[j][1] - p[i][1]) > 0))) f3++; //排除掉共线的多余点   
-//                }
-//                if (((f1 == 0) || (f2 == 0)) && (f3 == 0))
-//                {
-//                    d = sqrt(pow((double)(p[k][0] - p[i][0]), 2.0) + pow((double)(p[k][1] - p[i][1]), 2.0));
-//                    total += d;
-//                    //printf("%f,%d,%d\n",d,f1,f2);  
-//                    //system("pause");  
-//                }
-//
-//            }
-//        }
-//    }
-//    printf("%lf\n", total);
-//}
+#define _CRT_SECURE_NO_WARNINGS 1
+#define PI 3.1415926
+#include <stdio.h>
+#include <math.h>
+
+int x[6], y[6], n, vir[6] = {0};
+double len[6][6] = { 0 };
+double R[6] = { 0 };
+int x1, x2, y3, y2;
+double max;
+int min(int a, int b)
+{
+	return a > b ? b : a;
+}
+
+void dfs(int step, double s)//step为牛奶的滴速，s为目前牛奶面积
+{
+	int i, k;
+	double r = 0;
+
+	if (step == n + 1)
+	{
+		max = s > max ? s : max;
+	}
+	else
+	{
+		for (k = 0; k < n; k++)
+		{
+			if (!vir[k])
+			{
+				r = min(min(abs(x[k] - x1), abs(x[k] - x2)), min(abs(y[k] - y3), abs(y[k] - y2)));
+				for (i = 0; i < n; i++)
+					if (vir[i])
+						if (r > len[i][k] - R[i])
+							r = len[i][k] - R[i];
+				r = r < 0 ? 0 : r;
+				vir[k] = 1;
+				R[k] = r;
+				dfs(step + 1, s + PI * r * r);
+				vir[k] = 0;
+				R[k] = 0;
+			}
+		}
+	}
+}
+
+int main()
+{
+	int a[6];
+	scanf("%d", &n);
+	scanf("%d %d %d %d", &x1, &y3, &x2, &y2);
+	for (int i = 0; i < n; i++)
+	{
+		scanf("%d %d", &x[i], &y[i]);
+	}
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < i; j++)
+		{
+			if (i != j)
+			{
+				len[i][j] = len[j][i] = sqrt(1.0 * (x[i] - x[j]) * (x[i] - x[j]) + 1.0 * (y[i] - y[j]) * (y[i] - y[j]));
+			}
+		}
+	}
+	int S = abs(x1 - x2) * abs(y3 - y2);
+	dfs(1, 0.0);
+	printf("%d\n", (int)(S - max + 0.5));
+	return 0;
+}
