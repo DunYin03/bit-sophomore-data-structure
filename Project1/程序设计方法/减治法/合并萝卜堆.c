a90c80865c8bbe28a0d9c08893c90b56b3161e8c
mayicheng <3228443778@qq.com>
2022-10-02 10:38:28 +0800

    查找最小值的优化

--- Project1/程序设计方法/减治法/合并萝卜堆.c
+++ Project1/程序设计方法/减治法/合并萝卜堆.c
@@ -1,82 +1,54 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #define _CRT_SECURE_NO_WARNINGS 1
 #include <stdio.h>
+#include <stdlib.h>
 
 int n;
-long long a[100000] = {0};
+long long a[100000];
 
-void swap(int i, int j)
+void find(long long x,long long n)
 {
-	long long temp = a[i];
-	a[i] = a[j];
-	a[j] = temp;
-}
-
-void qsort(long long a[], int start, int end)    //start和end都是指下标
-{
-	int i, j;
-	long long tmp,temp;
-
-	i = start;
-	j = end;
-	tmp = a[start];   //任命为中间分界线，左边比他小，右边比他大,通常第一个元素是基准数
-
-	if (i > j)  //如果下标i大于下标j，函数结束运行
-	{
-		return;
-	}
-
-	while (i != j)
-	{
-		while (a[j] >= tmp && j > i)
-		{
-			j--;
-		}
-
-		while (a[i] <= tmp && j > i)
-		{
-			i++;
-		}
-
-		if (j > i)
-		{
-			temp = a[j];
-			a[j] = a[i];
-			a[i] = temp;
-		}
-	}
-
-	a[start] = a[i];
-	a[i] = tmp;
-
-	qsort(a, start, i - 1);
-	qsort(a, i + 1, end);
+    long long y, tem;
+    while (x * 2 <= n)
+    {
+        y = x * 2;
+        if (y + 1 <= n && a[y + 1] < a[y]) y++;
+        if (a[x] > a[y])
+        {
+            tem = a[x];
+            a[x] = a[y];
+            a[y] = tem;
+            x = y;
+        }
+        else break;
+    }
 }
 
 int main()
 {
-	long long sum = 0;
-	scanf("%d", &n);
-	for (int i = 0; i < n; i++)
-	{
-		scanf("%lld", &a[i]);
-	}
-	qsort(a, 0, n - 1);
-	if (n == 0)
-		printf("%lld\n", a[0]);
-	else
-	{
-		for (int i = 0; i < n - 1; i++)
-		{
-			a[i + 1] += a[i];
-			sum += a[i + 1];
-			for (int k = i + 1; k < n; k++)
-				if (a[k] > a[k + 1])
-					swap(k, k + 1);
-				else
-					break;
-		}
-		printf("%lld\n", sum);
-	}
+    long long sum = 0;
+    long long temp = 0;
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++)
+    {
+        scanf("%lld", &a[i]);
+    }
+    for(int i = n/2;i>=1;i--)
+        find(i, n);
+    if (n == 0)
+        printf("%lld\n", a[0]);
+    else
+    {
+        for (int i = 1; i < n; i++)
+        {
+            temp = a[1];
+            a[1] = a[n - i + 1];
+            find(1, n - i);
+            sum += a[1] + temp;
+            a[1] += temp;
+            find(1, n - i);
+        }
+        printf("%lld\n", sum);
+    }
 
 }
