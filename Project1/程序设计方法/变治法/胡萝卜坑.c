75665fd71a0da6c8ee66244c428e8d3435bebe1b
mayicheng <3228443778@qq.com>
2022-10-10 14:45:52 +0800

    找规律，问题简化

--- Project1/程序设计方法/变治法/胡萝卜坑.c
+++ Project1/程序设计方法/变治法/胡萝卜坑.c
@@ -1,1 +1,47 @@
 #define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+int search(int n,int m)
+{
+	if (2 == n)
+	{
+		if (1 == m)
+		{
+			return 3;
+		}
+		else
+		{
+			return 4;
+		}
+	}
+	else if (3 <= n)
+	{
+		if (1 == m)
+		{
+			return 4;
+		}
+		else if (2 == m)
+		{
+			return 7;
+		}
+		else
+		{
+			return 8;
+		}
+	}
+}
+
+int main()
+{
+	int n, m;
+	scanf("%d%d", &n, &m);
+	if (0 == m)
+		printf("1\n");
+	else if (1 == n)
+		printf("2\n");
+	else
+	{
+		printf("%d\n", search(n, m));
+	}
+	return 0;
+}
