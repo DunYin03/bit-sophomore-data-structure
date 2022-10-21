e3cf216e611aff1fffafacca41a84926470e01a1
mayicheng <3228443778@qq.com>
2022-10-21 15:24:34 +0800

    完全背包

--- Project1/程序设计方法/综合练习1/投资.c
+++ Project1/程序设计方法/综合练习1/投资.c
@@ -1,1 +1,32 @@
 #define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <stdlib.h>
+int a[10], b[10], f[10000000];
+int main()
+{
+	int s, d, n;
+	scanf("%d%d%d", &s, &n, &d);
+
+	for (int i = 1; i <= d; i++)
+	{
+		scanf("%d%d", &a[i], &b[i]);
+		a[i] /= 1000;
+	}
+
+	while (n--)
+	{
+		
+		for (int i = 1; i <= d; i++)
+		{
+			for (int j = a[i]; j <= s/1000; j++)
+			{
+				if (f[j] < f[j - a[i]] + b[i])
+					f[j] = f[j - a[i]] + b[i];
+			}
+		}
+		s += f[s/1000];
+	}
+	printf("%d\n", s);
+
+	return 0;
+}
