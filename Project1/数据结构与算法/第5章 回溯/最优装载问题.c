779db9161663c1e05489b4a103cc736a55e87243
mayicheng <3228443778@qq.com>
2022-10-28 09:54:25 +0800

    回溯经典

--- Project1/数据结构与算法/第5章 回溯/最优装载问题.c
+++ Project1/数据结构与算法/第5章 回溯/最优装载问题.c
@@ -1,23 +1,51 @@
-//#define _CRT_SECURE_NO_WARNINGS 1
-//#include <stdio.h>
-//#include <stdlib.h>
-//int n, c, w[105];
-//int vis[105] = { 0 };
-//void trackback(int i,int rest)
-//{
-//	if (rest == 0)
-//	{
-//		
-//	}
-//}
-//
-//int main()
-//{
-//	scanf("%d%d", &c, &n);
-//	for (int i = 1; i <= n; i++)
-//	{
-//		scanf("%d", &w[i]);
-//	}
-//	trackback(1, c);
-//
-//}
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <stdlib.h>
+int n, c, w[105];
+int vis[105] = { 0 };
+int max = 0;
+
+void trackback(int i,int wei)
+{
+	if (c == wei)
+	{
+		printf("%d\n", c);
+		for (int i = 1; i <= n; i++)
+			if (vis[i])
+				printf("%d ", i);
+		printf("\n");
+		exit(0);
+	}
+	if (wei > c)
+	{
+		if (max < wei - w[i])
+			max = wei - w[i];
+		return;
+	}
+	for (int j = i; j <= n; j++)
+	{
+		if (!vis[j])
+		{
+			vis[j] = 1;
+			trackback(j, wei + w[j]);
+			vis[j] = 0;
+		}
+	}
+}
+
+int main()
+{
+	scanf("%d%d", &c, &n);
+	for (int i = 1; i <= n; i++)
+	{
+		scanf("%d", &w[i]);
+	}
+	trackback(1, w[0]);
+	printf("%d\n", max);
+	for (int i = 1; i <= n; i++)
+		if(vis[i])
+			printf("%d ", i);
+	printf("\n");
+
+	return 0;
+}
