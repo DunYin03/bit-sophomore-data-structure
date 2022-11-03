776d85a1adc25e87a9309b75f9c59fa2b32493df
mayicheng <3228443778@qq.com>
2022-11-03 21:39:45 +0800

    无向图的各连通分支

--- Project1/数据结构与算法/第6章 图/无向图的各连通分支.c
+++ Project1/数据结构与算法/第6章 图/无向图的各连通分支.c
@@ -1,1 +1,79 @@
 #define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include <stdlib.h>
+
+int record[100] = { 0 };
+typedef struct
+{
+	int id;
+	int num;
+	int link[100];
+}node;
+
+int cmp(const void* a, const void* b)
+{
+	return *(int*)a - *(int*)b;
+}
+
+node dot[100];
+
+void bfs(int pt)
+{
+	int flag = 1;
+	int num = 0;
+	qsort(dot[pt].link, dot[pt].num, sizeof(int), cmp);
+	for (int i = 0; i < dot[pt].num; i++)
+	{
+		if (dot[dot[pt].link[i]].id == 0)
+		{
+			flag = 0;
+			printf("-%d", dot[pt].link[i]);
+			dot[dot[pt].link[i]].id = 1;
+			record[num] = dot[pt].link[i];
+			num++;
+		}
+	}
+	if (flag)
+		return;
+	for (int i = 0; i < num; i++)
+	{
+		bfs(record[i]);
+	}
+	return;
+}
+
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++)
+	{
+		dot[i].id = 0;
+		dot[i].num = 0;
+	}
+
+	int f, t;
+	while (1)
+	{
+		scanf("%d", &f);
+		if (f == -1)
+			break;
+		scanf("%d", &t);
+		dot[f].link[dot[f].num] = t;
+		dot[f].num++;
+		dot[t].link[dot[t].num] = f;
+		dot[t].num++;
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		if (dot[i].id == 0)
+		{
+			printf("%d", i);
+			dot[i].id = 1;
+			bfs(i);
+			printf("\n");
+		}
+	}
+	return 0;
+}
